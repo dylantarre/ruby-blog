f508644242545d8e11bf0f715ecd5a1312b25e1c
dylantarre <me@dylantarre.com>
2016-09-28 21:00:26 -0700

    Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927234449) do
+ActiveRecord::Schema.define(version: 20160928174559) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
 end
